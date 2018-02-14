94ce15e40bdc7112180d310375b194fe7043dfe3
root <root@ubuntu16-micro.cloud.compas.cs.stonybrook.edu>
2018-02-14 18:41:30 +0000

    PHP now calling JS function.

--- ttt/index.php
+++ ttt/index.php
@@ -1,18 +1,27 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="style1.css">
+<script>
+function loadBoard() {
+    
+}
+</script>
 </head>
 <h1>Are you ready to play?</h1>
    <body>  
       <form action = "<?php $_PHP_SELF ?>" method = "post">
          Name: <input type = "text" name = "name" />
          <input type = "submit" />
-      </form>     
+      </form>
+
    </body>
-</html>
 
 <?php
-	if ($_POST["name"]) {
-		echo "Hello ". $_POST["name"], ", ", date(DATE_RFC822);
-	}
+if ($_POST["name"]) {
+    echo "Hello ". $_POST["name"], ", ", date(DATE_RFC822);
+    echo '<script> loadBoard(); </script>';
+}
 ?>
+</html>
+
+
