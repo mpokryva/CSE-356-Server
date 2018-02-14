9248f400fdc51680e6cde9decb75b51a70356938
root <root@ubuntu16-micro.cloud.compas.cs.stonybrook.edu>
2018-02-14 17:01:53 +0000

    Added node app.

---
+++ nodeapp/app.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+var N_ROWS = 3;
+var N_COLS = 3;
+var WIN_NUM = 3;
+app.use(bodyParser.json());
+var ip = require("ip");
+console.log(ip.address());
+app.post('/ttt/play', function(req, res) {
+        // Parse grid.
+        var grid = req.body["grid"];
+        console.log(grid);
+        var winner = checkWinner(grid);
+        console.log("The winner is:" + winner + ".");
+        var grid = makeMove(grid);
+        var response = {"grid": grid, "winner": winner};
+        res.send(response);
+});
+
+app.listen(8080, ip.address(), () => console.log('Example app listening on port 8080!'));
+	
+function makeMove(grid) {
+    for (var i = 0; i < grid.length; i++) {
+        if (grid[i] == " ") {
+            grid[i] = "O";
+            break;
+        }
+    }
+    return grid;
+}
+
+function checkWinner(grid) {
+    var winner = checkWinnerRC(grid, true);
+    console.log("Checking rows...");
+    if (winner == " ") {
+        winner = checkWinnerRC(grid, false);
+        console.log("Checking cols...");
+        if (winner == " ") {
+			console.log("Checking diagonals...");
+            winner = checkWinnerDiag(grid);
+            return winner;
+        } else {
+            return winner;
+        }
+    } else {
+        return winner;
+    }
+}
+
+function checkWinnerRC(grid, checkRows) {
+	// Check for winner in rows.
+    var outerStop = (checkRows) ? N_ROWS : N_COLS;
+    var innerStop = (checkRows) ? N_COLS : N_ROWS;
+    for (var i = 0; i < outerStop; i++) {
+        var winner = " ";
+        var prev = null;
+        for (var j = 0; j < innerStop; j++) {
+			var row = (checkRows) ? i : j;
+			var col = (checkRows) ? j : i;
+            var index = getIndex(row, col, N_ROWS, N_COLS);
+            if (grid[index] != " " && (prev == null || grid[index] == prev)) {
+                // Keep searching this row;
+                prev = grid[index];
+            } else {
+                break;
+            }
+            if (j + 1 == WIN_NUM) { // Found a winner!
+                console.log("Found a winner!");
+                return grid[index];
+            }
+        }
+    }
+    return " ";
+    
+}
+
+function checkWinnerDiag(grid) {
+    var topLeft = grid[getIndex(0, 0, N_ROWS, N_COLS)];
+    var topRight = grid[getIndex(0, 2, N_ROWS, N_COLS)];
+    var botLeft = grid[getIndex(2, 0, N_ROWS, N_COLS)];
+    var botRight = grid[getIndex(2, 2, N_ROWS, N_COLS)];
+    var center = grid[getIndex(1, 1, N_ROWS, N_COLS)];
+    if (topLeft != " " && topLeft == center && center == botRight) {
+        return topLeft;
+    } else if (topRight != " " && topRight == center && center == botLeft) {
+        return topRight;
+    } else {
+        return " ";
+    }
+}
+
+
+function getRow(index, nCols) {
+	return index / nCols;
+}
+
+function getIndex(row, col, nCols, nRows) {
+    return (row * nCols) + col;
+}
+
+function getCol(index, nCols) {
+	return index % nCols;
+}
+
