ad5750230a3986fdc0a0e9131776cc0ad005eb3f
root <root@cloud.compas.cs.stonybrook.edu>
2018-02-26 00:56:10 +0000

    user.js now sends emails.

--- nodeapp/user.js
+++ nodeapp/user.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const app = express()
-    var bodyParser = require('body-parser');
-    var N_ROWS = 3;
-    var N_COLS = 3;
-    var WIN_NUM = 3;
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
     app.use(bodyParser.json());
     app.post('/', function(req, res) {
-        res.send("Hello world!");
+        var body = req.body;
+        console.log(body);
+        var username = body.username;
+        console.log(username);
+        sendEmail();
+        //testEmail();
     });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
 
+
+function sendEmail(recepient, msgText) {
+    "use strict";
+    var hostname = "localhost"; // Could leave this out, but including it for clarity.
+    let transporter = nodemailer.createTransport({
+        host: hostname,
+        port: 25,
+        secure: false,
+        tls: { 
+            rejectUnauthorized: false
+        }
+    });
+    
+
+    let mailOptions = {
+        from: "<ubuntu@ubuntu16-micro.cloud.compas.cs.stonybrook.edu>",
+        to: recepient,
+        text: msgText
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+}
