667816d669a2631486030ae2b9a361513b55489e
root <root@cloud.compas.cs.stonybrook.edu>
2018-03-01 04:50:15 +0000

    getgame endpoint implemented.

--- nodeapp/game-info.js
+++ nodeapp/game-info.js
@@ -16,7 +16,14 @@ app.post('/listgames', function(req, res) {
         utils.sendStatus(err, res, pastGames);
     });
 });
-
+app.post("/getgame", (req, res) => {
+    const username = utils.getUsername(req);
+    if (!username) return utils.sendStatus({statusCode: 403}, res); // User not logged in.
+    const gameId = req.body.id;
+    getGame(username, gameId, (err, game) => {
+        utils.sendStatus(err, res, game);
+    }); 
+});
 app.listen(8003, () => console.log('Example app listening on port 8003!'));
 
 function getPastGames(username, callback) {
@@ -45,6 +52,35 @@ function getPastGames(username, callback) {
     });
 }
 
+function getGame(username, id, callback) {
+    const query = {_id: username};
+    const projection = {pastGames: 1};
+    const errCode = 404;
+    utils.mongoFindInUsers(query, projection, errCode, (err, res) => {
+        if (!err) {
+            // Found stuff.
+            console.log(res);
+            // Extract pastGames (bc projection isn't working).
+            var pastGames = res.pastGames;
+            var game = null;
+            if (pastGames) {
+                for (var i = 0; i < pastGames.length; i++) {
+                    pastGames[i] = pastGames[i].game; // Hacking around "game" key error.
+                    if (pastGames[i].id == id) { // Game found.
+                        game = pastGames[i];
+                        delete game.id;
+                        delete game.start_date;
+                        break;
+                    }
+                }
+            }
+            err = (game) ? null : {statusCode: errCode};
+            return callback(err, game);
+        } else {
+            return callback(err, res);
+        }
+    });
+}
 
 function generateKey(length) {
     "use strict";
